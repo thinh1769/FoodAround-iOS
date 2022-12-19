887e469cfa6643d6a2c76757cda703cae1c5e939
thinh1769 <thinhnp@rakumo.vn>
2022-12-19 17:22:32 +0700

    Initial Commit

---
+++ Food Around/Modules/SignIn/SignInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignInViewController.swift
+//  Food Around
+//
+//  Created by Nguyễn Thịnh on 19/12/2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+}
---
+++ Food Around/Modules/SignUp/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Food Around
+//
+//  Created by Nguyễn Thịnh on 19/12/2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+}
